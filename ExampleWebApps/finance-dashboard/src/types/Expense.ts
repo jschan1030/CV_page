8a1234c3e2076602eb55eaccfd5635720d5d1f24
Joseph Chan <jojosfdev@gmail.com>
2026-02-18 19:06:03 -0700

    removing files added by accident

--- ExampleWebApps/finance-dashboard/src/types/Expense.ts
+++
@@ -1,8 +0,0 @@
-export interface Expense {
-    id: number;
-    name: string;
-    amount: number;
-    type: 'fixed' | 'variable' | 'debt';
-    card: 'WF Credit Card' | 'WF Debit Card' | 'Chase Credit Card' | 'Sams Club' | 'Best Buy' | 'Cash' | 'Other';
-    category: 'housing' | 'food' | 'transportation' | 'utilities' | 'entertainment' | 'insurance' | 'loans' | 'other';
-}
--- ExampleWebApps/finance-dashboard/src/types/Spending.ts
+++
@@ -1,7 +0,0 @@
-export interface Spending {
-    id: number;
-    amount: number;
-    year: string; // e.g., "2025"
-    month: string; // e.g., "January"
-    name: string; // e.g., "January 2025"
-}
